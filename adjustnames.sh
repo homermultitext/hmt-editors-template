a5a07a98f3a4272225cb7c4dcc469f3067b355d4
Neel Smith <dnsmith.neel@gmail.com>
2014-06-22 16:27:27 -0400

    Set up mom config as template.

---
+++ adjustnames.sh
@@ -0,0 +1,7 @@
+# determine directory?  or require it to be supplied?
+# Substitute name of directory as required in various config files 
+# and scripts.
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Adjust name and path to ${DIR}"
